815de0b4245b080fa733323a6745de894dd07505
v.isakov <v.isakov@andersenlab.com>
2019-12-07 18:40:34 +0200

    print env vars

--- config/load_config.sh
+++ config/load_config.sh
@@ -3,7 +3,7 @@ DRIVE_URL=$1
 ARCHIVE=$2
 echo "______$DRIVE_URL____"
 echo "____________$ARCHIVE___________"
-curl curl -L -o config.zip "$DRIVE_URL" --output - \
+curl curl -L -o config.zip "$DRIVE_URL" \
     && unzip -P "$ARCHIVE" config.zip \
     && mv config/config.properties ./ \
     && mv config/tmdb_release.keystore ./config \
