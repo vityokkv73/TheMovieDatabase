d9f6a128f3d2e77a498653da40145391da259c97
v.isakov <v.isakov@andersenlab.com>
2019-12-07 18:46:52 +0200

    another one try

--- config/load_config.sh
+++ config/load_config.sh
@@ -7,8 +7,8 @@ echo "$2"
 
 echo "______$DRIVE_URL____"
 echo "____________$ARCHIVE___________"
-curl curl -L -o config.zip "$DRIVE_URL" \
-    && unzip -P "$ARCHIVE" config.zip \
+curl curl -L -o config.zip "$1" \
+    && unzip -P "$2" config.zip \
     && mv config/config.properties ./ \
     && mv config/tmdb_release.keystore ./config \
     && rm config.zip
