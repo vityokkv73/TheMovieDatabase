6d17957d846e6b2d70c288747bfcd130da07ff5b
v.isakov <v.isakov@andersenlab.com>
2019-12-07 18:11:58 +0200

    try to pass params to script via config.yml

--- config/load_config.sh
+++ config/load_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+DRIVE_URL=$1
+ARCHIVE=$2
 echo "______$DRIVE_URL____"
 echo "____________$ARCHIVE___________"
-
 curl curl -L -o config.zip "$DRIVE_URL" --output - \
     && unzip -P "$ARCHIVE" config.zip \
     && mv config/config.properties ./ \
