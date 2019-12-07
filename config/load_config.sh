8d4b769318a7130bb6521b8b869fca9bf770e1c2
v.isakov <v.isakov@andersenlab.com>
2019-12-07 19:21:04 +0200

    added release build flow

--- config/load_config.sh
+++ config/load_config.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-DRIVE_URL=$1
-ARCHIVE=$2
-echo "$0"
-echo "$1"
-echo "$2"
-
-echo "______$DRIVE_URL____"
-echo "____________$ARCHIVE___________"
-curl -L -o config.zip "$1" \
-    && unzip -P "$2" -o config.zip
-echo $PWD
+curl -L -o config.zip "$1"
+unzip -P "$2" -o config.zip
 mv tmdb_release.keystore ./config
 rm config.zip
