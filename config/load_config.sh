b7beaff10515edc59bda60b7e72a139411928937
v.isakov <v.isakov@andersenlab.com>
2019-12-07 17:00:16 +0200

    load config dynamically

--- config/load_config.sh
+++ config/load_config.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-curl -X POST https://content.dropboxapi.com/2/files/download_zip \
-    --header "Authorization: Bearer $DROPBOX_KEY" \
-    --header "Dropbox-API-Arg: {\"path\": \"/$CONFIG_ARCHIVE_NAME\"}" \
-    -o "./$CONFIG_ARCHIVE_NAME.zip" \
-    && unzip -o "$CONFIG_ARCHIVE_NAME.zip" \
-    && mv $CONFIG_ARCHIVE_NAME/api-config.properties ./ \
-    && mv $CONFIG_ARCHIVE_NAME/app/google-services.json ./app \
-    && rm "$CONFIG_ARCHIVE_NAME.zip"
+curl curl -L -o config.zip "$DRIVE_URL" \
+    && unzip -P "$ARCHIVE" config.zip \
+    && mv config/config.properties ./ \
+    && mv config/tmdb_release.keystore ./config \
+    && rm config.zip
