97101e5374fec8a2617a0818fc0f4f8cb9946aa0
v.isakov <v.isakov@andersenlab.com>
2019-12-07 17:31:43 +0200

    enable output of curl command

--- config/load_config.sh
+++ config/load_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl curl -L -o config.zip "$DRIVE_URL" \
+curl curl -L -o config.zip "$DRIVE_URL" --output - \
     && unzip -P "$ARCHIVE" config.zip \
     && mv config/config.properties ./ \
     && mv config/tmdb_release.keystore ./config \
