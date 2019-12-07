fafee7882e58529281bfb507109a5b0fc1729194
v.isakov <v.isakov@andersenlab.com>
2019-12-07 20:18:53 +0200

    load config dynamically + added release_build to workflow

--- config/load_config.sh
+++ config/load_config.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-curl -X POST https://content.dropboxapi.com/2/files/download_zip \
-    --header "Authorization: Bearer $DROPBOX_KEY" \
-    --header "Dropbox-API-Arg: {\"path\": \"/$CONFIG_ARCHIVE_NAME\"}" \
-    -o "./$CONFIG_ARCHIVE_NAME.zip" \
-    && unzip -o "$CONFIG_ARCHIVE_NAME.zip" \
-    && mv $CONFIG_ARCHIVE_NAME/api-config.properties ./ \
-    && mv $CONFIG_ARCHIVE_NAME/app/google-services.json ./app \
-    && rm "$CONFIG_ARCHIVE_NAME.zip"
+curl -L -o config.zip "$1"
+unzip -P "$2" -o config.zip
+mv tmdb_release.keystore ./config
+rm config.zip
